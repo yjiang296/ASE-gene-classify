cfe845eac053fd651aed20dc3afef68ba5d6d691
yjiang296 <115337217+yjiang296@users.noreply.github.com>
2023-08-24 10:51:35 +0800

    Update chisq_BH.R

--- chisq_BH.R
+++ chisq_BH.R
@@ -2,29 +2,28 @@ setwd('/data21/yjiang22/new_hybrid_hap_graph_BM')
 
 args <- commandArgs(T)
 classification.wo0.csv <- as.character(args[1])
-#classification.wo0_fdr.csv <- as.character(args[2])
 out_BH <- as.character(args[2])
-# 加载所需的包
+
+# load package
 library(tidyverse)
 
-# 读取文件，假设你的文件叫"data.csv"
 data <- read.csv(classification.wo0.csv,header=T,sep='\t')
 
-# 执行卡方检验并将结果添加为新列
+# Perform a chi-square test and add the result as a new column
 data <- data %>% 
   mutate(chisq_test = pmap(list(hap1_count, hap2_count), function(hap1, hap2) {
     test <- chisq.test(c(hap1, hap2))
     return(test$p.value)
   }))
 
-# 将 chisq_test 列的内容转化为数字
+# change chisq_test colunm data type to numerical
 data$chisq_test <- as.numeric(data$chisq_test)
 
-# 用Benjamini & Hochberg方法对p值进行校正
+# use Benjamini & Hochberg method to adjust p value
 data <- data %>% 
   mutate(p_adj = p.adjust(chisq_test, method = "BH"))
 
-# 写入新的CSV文件
+# write into a new CSV
 write_csv(data, out_BH)
 
-##usage:    >Rscript chisq_BH.R <input *count.csv> <output *count.BHcsv>
+##usage:    >Rscript chisq_BH.R <input *count.csv> <output *count.BH.csv>
